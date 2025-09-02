c0fd8904cb14626552c7a1148ec4694558ab7d4e
Gugle <administration@ztxy666.cn>
2025-09-02 18:49:38 +0800

    fix(权限控制): 更新 transfer 命令权限检查逻辑
    - 修改 TransferCommandMixin 类中的权限检查逻辑
    - 更新 gradle.properties 文件中的 rolling_gate_version 属性到 1.1.0+build.62

--- src/main/java/dev/anvilcraft/rg/server/mixin/TransferCommandMixin.java
+++ src/main/java/dev/anvilcraft/rg/server/mixin/TransferCommandMixin.java
@@ -6,6 +6,8 @@ import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import dev.anvilcraft.rg.server.ServerPlusPlusServerRules;
 import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.PermissionSource;
+import net.minecraft.server.commands.PermissionCheck;
 import net.minecraft.server.commands.TransferCommand;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,13 +15,40 @@ import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(TransferCommand.class)
 abstract class TransferCommandMixin {
-    @WrapOperation(method = "lambda$register$0", at = @At(value = "INVOKE", target = "Lnet/minecraft/commands/CommandSourceStack;hasPermission(I)Z"))
-    private static boolean registerPermission(CommandSourceStack instance, int i, Operation<Boolean> original) {
-        return ServerPlusPlusServerRules.commandTransfer || original.call(instance, i);
+    @WrapOperation(
+        method = "register", at = @At(
+        value = "INVOKE", target = "Lnet/minecraft/commands/Commands;hasPermission(I)Lnet/minecraft/server/commands/PermissionCheck;"
+    )
+    )
+    private static <T extends PermissionSource> @NotNull PermissionCheck<T> registerPermission(
+        int level,
+        Operation<PermissionCheck<T>> original
+    ) {
+        return ServerPlusPlusServerRules.commandTransfer ? new PermissionCheck<>() {
+            @Override
+            public int requiredLevel() {
+                return 0;
+            }
+
+            @Override
+            public boolean test(T t) {
+                return true;
+            }
+        } : original.call(level);
     }
 
-    @WrapOperation(method = "register", at = @At(value = "INVOKE", target = "Lnet/minecraft/commands/Commands;argument(Ljava/lang/String;Lcom/mojang/brigadier/arguments/ArgumentType;)Lcom/mojang/brigadier/builder/RequiredArgumentBuilder;", ordinal = 2))
-    private static <T> @NotNull RequiredArgumentBuilder<CommandSourceStack, T> register(String string, ArgumentType<T> argumentType, @NotNull Operation<RequiredArgumentBuilder<CommandSourceStack, T>> original) {
+    @WrapOperation(
+        method = "register", at = @At(
+        value = "INVOKE",
+        target = "Lnet/minecraft/commands/Commands;argument(Ljava/lang/String;Lcom/mojang/brigadier/arguments/ArgumentType;)Lcom/mojang/brigadier/builder/RequiredArgumentBuilder;",
+        ordinal = 2
+    )
+    )
+    private static <T> @NotNull RequiredArgumentBuilder<CommandSourceStack, T> register(
+        String string,
+        ArgumentType<T> argumentType,
+        @NotNull Operation<RequiredArgumentBuilder<CommandSourceStack, T>> original
+    ) {
         return original.call(string, argumentType).requires(stack -> stack.hasPermission(3));
     }
 }
